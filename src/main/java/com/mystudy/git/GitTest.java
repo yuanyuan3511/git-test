5ba94b93d6a6cd3758fb2a055a5ff088f4823d12
wcygenji <wcy1421913058@163.com>
2022-09-26 21:56:04 +0800

    third commit git

--- src/main/java/com/mystudy/git/GitTest.java
+++ src/main/java/com/mystudy/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git idea");
         System.out.println("hello git idea");
+        System.out.println("hello git idea3");
     }
 
 }
