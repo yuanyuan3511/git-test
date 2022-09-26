c17aba62c61db3a5ea284d987d034163beb284a9
wcygenji <wcy1421913058@163.com>
2022-09-26 22:29:32 +0800

    hot-fix 3 commit git

--- src/main/java/com/mystudy/git/GitTest.java
+++ src/main/java/com/mystudy/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git idea3");
         System.out.println("hello git idea3 hot-fix");
         System.out.println("hello git idea3 hot-fix222222");
+        System.out.println("hello git idea3 hot-fix222222");
     }
 
 }
