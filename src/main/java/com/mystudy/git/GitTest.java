51848c2ad212eb55e59d250ea666cbf11e9a0983
wcygenji <wcy1421913058@163.com>
2022-09-26 16:58:21 +0800

    first commit git

---
+++ src/main/java/com/mystudy/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.mystudy.git;
+
+/**
+ * @author wcystart
+ * @create 2022-09-26
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git idea");
+    }
+
+}
